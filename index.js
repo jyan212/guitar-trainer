0ba222a6d30f202149d05170f83d50ea15dab5e2
Jun Yan <48308945+jyan212@users.noreply.github.com>
2019-04-10 18:42:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import s from 'socket.io';
 const socket = s.io();
-
+var msg;
+socket.on('msg',(a)=>{
+    msg = a;
+})
 export default class App extends React.Component {
     render(){
         return(
             <div>
                 <p>{
-                    socket.on(
-                        'msg',(a)=>{return a}
-                    )
+                 a   
+                }
                 }</p>
             </div>
         )
